ee71ef83a0d20f736389f8362e80b15226cc9ef5
deepak <Deepak.Krishnegowda@ust-global.com>
2020-07-19 18:25:48 +0530

    added React-navigation

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import DashboardView from './Source/DashboardView';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => DashboardView);
+AppRegistry.registerComponent(appName, () => App);
